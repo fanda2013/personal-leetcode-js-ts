9c780b7d558e1a0e716328a60568018bb2c3b1e6
fanda2013 <179030867@qq.com>
2020-08-27 11:27:05 +0800

    70

---
+++ 70. Climbing Stairs.ts
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let res = {}
+
+    const go = n => {
+        if (n <= 3) return n
+
+        if (res[n] === undefined) {
+            res[n] = go(n - 1) + go(n - 2)
+        }
+
+        return res[n]
+    }
+
+    return go(n)
+};
