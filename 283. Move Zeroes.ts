8618c0ac58fc491e254924c48b7153461e562030
fanda2013 <179030867@qq.com>
2021-04-08 11:55:36 +0800

    redo 21 283 448

--- 283. Move Zeroes.ts
+++ 283. Move Zeroes.ts
@@ -4,10 +4,14 @@
  */
 var moveZeroes = function (nums) {
     for (let i = 0; i < nums.length; i++) {
+        let swap = false
         for (let j = 0; j < nums.length - i - 1; j++) {
             if (nums[j] === 0) {
                 [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+                swap = true
             }
         }
+
+        if (swap === false) break
     }
 };
