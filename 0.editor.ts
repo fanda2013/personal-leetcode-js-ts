f3df99fd708a65d530430773a9d70256295436b1
mpx2m <mpx2m2@163.com>
2021-04-10 23:58:00 +0800

    just test amend

--- 0.editor.ts
+++ 0.editor.ts
@@ -4,7 +4,7 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
- * }
+ * }11
  */
 /**
  * @param {TreeNode} root
