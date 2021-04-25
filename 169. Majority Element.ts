d7875e5e9d53d9f611ec7d49ae5c4076dd8e840a
mpx2m <mpx2m2@163.com>
2021-04-25 12:52:33 +0800

    redo 108 169 242

--- 169. Majority Element.ts
+++ 169. Majority Element.ts
@@ -4,10 +4,8 @@
  */
 var majorityElement = function (nums) {
     const obj = {}
-
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-        obj[num] = ++obj[num] || 1
-        if (obj[num] > nums.length / 2) return nums[i]
+        obj[nums[i]] = ++obj[nums[i]] || 1
+        if (obj[nums[i]] > nums.length / 2) return nums[i]
     }
 };
