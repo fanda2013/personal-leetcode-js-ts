73bf63349c3a39c311b7e84bbd6a5a4dfa025618
mpx2m <mpx2m2@163.com>
2021-05-02 08:34:36 +0800

    redo 13 217 21

--- 0.editor.ts
+++ 0.editor.ts
@@ -1,15 +1,25 @@
 /**
- * @param {string} columnTitle
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var titleToNumber = function (columnTitle) {
-    const arr = columnTitle.split("").reverse()
-    let num = 0
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
 
-    arr.forEach((char, i) => {
-        const value = (char.charCodeAt(0) - 64) * (26 ** i)
-        num += value
-    });
+    if (!l1) return l2
+    if (!l2) return l1
 
-    return num
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
 };
--- 13. Roman to Integer.ts
+++ 13. Roman to Integer.ts
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-    const map = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    const obj = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
 
-    let num = 0
+    let res = 0
 
     for (let i = 0; i < s.length; i++) {
-        const curr = map[s[i]]
-        const next = map[s[i + 1]]
+        const curr = obj[s[i]]
+        const next = obj[s[i + 1]]
 
-        curr < next ? num -= curr : num += curr
+        next > curr ? res -= curr : res += curr
     }
 
-    return num
+    return res
 };
