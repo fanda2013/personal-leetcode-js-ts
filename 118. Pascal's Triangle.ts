b8b026aac493ed0e25e29fc439b6139e9cb5b814
mpx2m <mpx2m2@163.com>
2021-05-08 16:42:57 +0800

    redo 118 268 191

--- 118. Pascal's Triangle.ts
+++ 118. Pascal's Triangle.ts
@@ -3,7 +3,8 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
-    const result = [];
+    const result = []
+
     if (numRows > 0) result.push([1])
     if (numRows > 1) result.push([1, 1])
 
@@ -14,7 +15,7 @@ var generate = function (numRows) {
             if (j === 1 || j === i) {
                 arr.push(1)
             } else {
-                const number = result[i - 2][j - 2] + result[i - 2][j - 1]
+                const number = result[i - 2][j - 1] + result[i - 2][j - 2]
                 arr.push(number)
             }
         }
--- 268. Missing Number.ts
+++ 268. Missing Number.ts
@@ -5,7 +5,9 @@
 var missingNumber = function (nums) {
     const arr = new Array(nums.length + 1).fill(-1)
 
-    for (let num of nums) arr[num] = num
+    nums.forEach(num => {
+        arr[num] = num
+    });
 
     return arr.indexOf(-1)
-};
+}
